5d553bbf3e6b04479468183a2824f0a09d161ec3
Evaldo Gioseffi <evaldogioseffi075@gmail.com>
2026-09-09 13:35:28 -0300

    Add files via upload

---
+++ Fund_Programação/QuadradoCuboRaizQuadrada/src/pacote/App.java
@@ -0,0 +1,29 @@
+package pacote;
+
+import java.util.Scanner;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		Scanner leia = new Scanner(System.in);
+		
+		int numero;
+		
+		double raiz;
+		
+		do {
+			System.out.println("Digite um número: ");
+			numero = leia.nextInt();
+			
+			raiz = Math.sqrt(numero);
+			
+			System.out.println("Valor digitado: " + numero);
+			System.out.println("Quadrado do número: " + (numero * numero));
+			System.out.println("Cubo do número: " + (numero * numero * numero));
+			System.out.println("Raiz quadrada do número: " + Math.round(raiz));
+		} while (numero > 0);
+
+	}
+
+}
